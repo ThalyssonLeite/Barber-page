eb4e758cae1c358a716bd7608ce6775ad201d277
Thalysson Leite <thalyssonl17@gmail.com>
2021-07-29 11:40:51 -0300

    Everything done, project complete

--- scripts/main.js
+++ scripts/main.js
@@ -13,20 +13,74 @@ menuLinks.forEach(element => {
     });
 });
 
+
 const header = document.querySelector('#header');
 const headerHeight = header.offsetHeight;
-
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
+    
     if (window.scrollY >= headerHeight) {
         header.classList.add('scroll');
     } else {
         header.classList.remove('scroll');
     }
+}
+
+
+const backToTop = document.querySelector('.back-to-top');
+function changeBackToTopButton() {
+    
+    if (window.scrollY >= 560) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+}
+
+const sections = document.querySelectorAll('.section');
+function activateMenuAtCurrentSection() {
+    const checkpoint = (innerHeight / 2) + scrollY;
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute('id');
+
+        const upperLimit = checkpoint >= sectionTop;
+        const lowerLimit = checkpoint <= sectionTop + sectionHeight;
+
+        if (upperLimit && lowerLimit) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active');
+        }
+    });
+}
+
+
+
+window.addEventListener('scroll', () => {
+    changeHeaderWhenScroll();
+    changeBackToTopButton();
+
+    //if (largeScreen) {
+    activateMenuAtCurrentSection();
+    //}
 });
 
 //TESTIMONIAL SWIPER LIB
+const largeScreen = screen.width >= 990;
+
+function slidesPerDeviceWidth() {
+
+    if (largeScreen) {
+        return 2
+    } else {
+        return 1
+    }
+}
+
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
+    slidesPerView: slidesPerDeviceWidth(),
     pagination: {
         el: '.swiper-pagination'
     },
@@ -42,10 +96,24 @@ const scrollReveal = ScrollReveal({
     reset: true
 });
 
-scrollReveal.reveal(`
-    #home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services .cards, #services .card-wrapper,
-    #contact header, #contact .links, #contact .links .logo, #contact .link li,
-    #footer a, #footer .text, #footer .links
-`, { interval: 100 });
+function scrollRevealDeviceWidth() {
+    if (!largeScreen) {
+        return `
+        #home .image, #home .text,
+        #about .image, #about .text,
+        #services header, #services .cards, #services .card-wrapper,
+        #contact header, #contact .links, #contact .links .logo, #contact .link li,
+        #footer a, #footer .text, #footer .links
+    `
+    } else {
+        return `
+        #home,
+        #about,
+        #services header, #services .cards, #services .card-wrapper,
+        #contact header, #contact .links, #contact .links .logo, #contact .link li,
+        #footer a, #footer .text, #footer .links
+    `
+    }
+}
+
+scrollReveal.reveal(scrollRevealDeviceWidth(), { interval: 100 });
